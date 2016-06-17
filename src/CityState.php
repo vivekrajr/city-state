3236dc43004bd074d8221d13c3b365c384499e0b
Vivek Raj <vivekrajr@live.com>
2016-06-17 14:46:02 +0200

    Generated countries

--- src/CityState.php
+++ src/CityState.php
@@ -1,19 +1,36 @@
 <?php namespace Vivek\CityState;
 
+require_once 'vendor/autoload.php';
+
 use ZipArchive;
+use \Symfony\Component\Yaml\Yaml;
 
 class CS {
     
     private static $MAXMIND_ZIPPED_URL = "http://geolite.maxmind.com/download/geoip/database/GeoLite2-City-CSV.zip";
     private static $MAXMIND_DB_FILE = "../db/GeoLite2-City-Locations-en.csv";
+    private static $COUNTRIES_FILE = "../db/countries.yml";
+    
+    //CSV positions
+    private static $ID = 0;
+    private static $COUNTRY = 4;
+    private static $COUNTRY_LONG = 5;
+    private static $STATE = 6;
+    private static $STATE_LONG = 7;
+    private static $CITY= 10;
+    
+    private static $countries = array();
     
     public static function updateMaxMind() {
+        echo "Downloading file\n";
         //file_put_contents("../db/CityState.zip", fopen("http://geolite.maxmind.com/download/geoip/database/GeoLite2-City-CSV.zip", 'r'));
     
         $zip = new ZipArchive;
         
         $enFile;
         
+        
+        echo "Looking for CSV file\n";
         if ($zip->open("../db/CityState.zip") === TRUE) {
             for($i = 0; $i < $zip->numFiles; $i++) {
                 $stat = $zip->statIndex( $i );      
@@ -29,18 +46,60 @@ class CS {
         copy("../db/".$enFile, "../db/".$fileName); 
     }
     
-    public static function generateCountries() {
-        if(($handle = fopen(self::$MAXMIND_DB_FILE, "r")) !== FALSE) {
-            while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-                $num = count($data);
-                    echo "<p> $num fields in line $row: <br /></p>\n";
-                    $row++;
-                for ($c=0; $c < $num; $c++) {
-                    echo $data[$c] . "<br />\n";
+    
+    public static function update() {
+        self::updateMaxMind();
+    }
+    
+    
+    //List all countries in the world from countries.yml
+    public static function countries() {
+        if(!file_exists(self::$COUNTRIES_FILE)) {
+            //Whoops. No countries.yml file. Get it from MAXMIND_DB_FILE
+            echo "No countries.yml file. \n";
+            if(!file_exists(self::$MAXMIND_DB_FILE)) {
+                //Whoops. MAXMIND_DB_FILE does not exist. Download and extract from zip
+                echo "No Maxmind CSV file. Redownloading";
+                
+                //TODO: Download and extract the CSV file;
+                
+            }
+            
+            $handle = fopen(self::$MAXMIND_DB_FILE, 'r');
+                
+            while (($line = fgets($handle)) !== false) {
+                $rec = explode(",", $line);
+                
+                if(empty($rec[self::$COUNTRY]) || empty($rec[self::$COUNTRY_LONG]) || strlen($rec[self::$COUNTRY]) > 2) {
+                    continue;
                 }
-            }    
+               
+                $country = strtoupper($rec[self::$COUNTRY]);
+                            
+                if(!array_key_exists($country, self::$countries)) {
+                    self::$countries[$country] = str_replace("\"", "", $rec[self::$COUNTRY_LONG]);
+                }
+ 
+            }
+            
+            ksort(self::$countries);
+            
+            $yaml = Yaml::dump(self::$countries);
+            file_put_contents(self::$COUNTRIES_FILE, $yaml);       
+            echo "Done reading DB and generating countries\n";
+            
+            
+           
+        //End logic to generate countries.yml
+        } else {
+            //File exists. just read from YAML
+            self::$countries = Yaml::parse(file_get_contents(self::$COUNTRIES_FILE));
         }
     }
 }
 
-CS::generateCountries();
+//CS::updateMaxMind();
+
+//CS::generateCountries();
+
+CS::countries();
